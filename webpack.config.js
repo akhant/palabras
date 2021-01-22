761c1aef2c1e1ec1da52b4fdc4a692f11ebb1bd3
anton <a.khantelev@mail.ru>
2021-01-22 21:47:36 +0300

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: './index.js',
+    main: ['./index.js'],
   },
   mode: 'development',
   module: {
@@ -10,14 +10,45 @@ module.exports = {
       {
         test: /\.(js|tsx?)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-react',
-            '@babel/preset-typescript',
-            ['@babel/preset-env', {modules: false}],
-          ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            configFile: false,
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+              ['@babel/preset-env', {modules: false}],
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              // [
+              //   'module-resolver',
+              //   {
+              //     alias: {
+              //       'react-native-gesture-handler': '../',
+              //       'react-native$': 'react-native-web',
+              //     },
+              //   },
+              // ],
+            ],
+          },
         },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader?limit=1000000',
+          },
+        ],
       },
     ],
   },
@@ -25,10 +56,12 @@ module.exports = {
     port: 8080,
     hot: true,
     contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
   },
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx'],
     alias: {
+      'react-native-gesture-handler': '../',
       'react-native$': 'react-native-web',
     },
   },
